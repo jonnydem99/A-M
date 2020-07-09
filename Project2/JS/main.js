ae1c4625d477ab434b1de6f2c62d6534f2adf7bf
Jonathan Niess <niessj@gmail.com>
2020-07-09 17:24:49 -0500

    4

--- Project2/JS/main.js
+++ Project2/JS/main.js
@@ -7,7 +7,9 @@ $('document').ready(()=>{
         $(gifs).empty();
         var userInput = $('#search').val();
         var numberOfGifs = $('#numSearchGifs').val();
+
         
+
         if (userInput == 0) {
             alert("Please fill in the 'GIF Search' field. If you leave the 'Number of GIFs' field blank the search will return a default of 25 GIFs.")
         };
@@ -16,19 +18,26 @@ $('document').ready(()=>{
             url: `http://api.giphy.com/v1/gifs/search?q=${userInput}&api_key=C3vgfkJ9NO78ZdGLXlm5cuODWh4UuI2W&limit=${numberOfGifs}`
             
         })
-            .done((res)=>{
+                
+        .done((res)=>{
                 var giphySearch = res.data;
                 $.each(giphySearch, (i, e)=>{
                     //console.log("index", i, "element", e)
                     //console.log(giphySearch)
                     var giphy = e.images.downsized_medium.url;
                     //console.log("giphy", giphy);
-                    $("#gifs").append(`<img src="${giphy}" alt="gyphy image"/>`)
+                    $("#gifs").append(`<img src="${giphy}" alt="gyphy image"/>`);
+                    
                 })
+                /* setTimeout(addBorder, 1000);
+        function addBorder() {
+            if (('#gif').val !== "") {
+                $("#gifs").addClass('activeBorder');
+            }
+        }*/
             })
-        
+            
     }
         
-    
     
 })
