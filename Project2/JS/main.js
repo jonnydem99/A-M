fe1d2c8ca204652f51a2ed1f219de71b53d067e8
Jonathan Niess <niessj@gmail.com>
2020-07-12 21:49:22 -0500

    Cleaned up code added comments

--- Project2/JS/main.js
+++ Project2/JS/main.js
@@ -1,42 +1,46 @@
+
+    //on document ready
 $('document').ready(()=>{
+        //functions on click of search button
     $('#searchbtn').click(()=>{
         searchGiphy();
         addBorder();
     })
-
+        //searches for gifs using Giphy API
     function searchGiphy() {
         $(gifs).empty();
         var userInput = $('#search').val();
         var numberOfGifs = $('#numSearchGifs').val();
         
-
+            //error message if a search is attempted with empty search criteria
         if (userInput == 0) {
             alert("Please fill in the 'GIF Search' field. If you leave the 'Number of GIFs' field blank the search will return a default of 25 GIFs.")
         };
-
+            //ajax call
         $.ajax({
             url: `http://api.giphy.com/v1/gifs/search?q=${userInput}&api_key=C3vgfkJ9NO78ZdGLXlm5cuODWh4UuI2W&limit=${numberOfGifs}`
             
         })
-                
+            //when ajax call is complete giphs are displayed
         .done((res)=>{
                 var giphySearch = res.data;
                 $.each(giphySearch, (i, e)=>{
-                    //console.log("index", i, "element", e)
-                    //console.log(giphySearch)
                     var giphy = e.images.downsized_medium.url;
-                    //console.log("giphy", giphy);
                     $("#gifs").append(`<img src="${giphy}" alt="gyphy image"/>`);
                     
                 })
+                    //after 1 second addBorder function is called to add border to gif search results
                 setTimeout(addBorder, 1000);
         
             })
             
     }
+    //if there is any content(gifs) inside the gif search results element, add activeBorder class to the element
     function addBorder() {
     if (('#gif').val !== "") {
         $("#gifs").addClass('activeBorder');
         }
     }   
+
+
 })
---
+++ Project2/README.md
@@ -0,0 +1,13 @@
+Jonathan Niess
+
+Project - To build a dynamic single page app that uses Giphy API to search and display gifs.
+
+Using the search input and search button, the user can search for gifs using keywords of his/her choice.
+
+Technologies Used - html, css, sass, jquery, javascript
+
+Ideas for future improvement - 
+	1. featured gifs section that displays popular gifs 
+	2. add functionality that allows 'return' or 'enter' key to trigger search
+	3. another page that has the ability to search memes
+
